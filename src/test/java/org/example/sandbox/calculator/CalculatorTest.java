3c8637ff0f70be94faf87add3246f96fcf32bb4e
Devon Bullock <dev2195587@maricopa.edu>
2024-08-29 19:44:50 -0700

    calculator testing

---
+++ src/test/java/org/example/sandbox/calculator/CalculatorTest.java
@@ -0,0 +1,29 @@
+package org.example.sandbox.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        assertEquals(10, Calculator.add(5,5));
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void divide() {
+    }
+}
